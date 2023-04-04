4c055fba35e66883f963382e71b198c0b411320f
adexoludare <adexoludare@gmail.com>
2023-04-03 19:27:45 -0700

    -python-more_classes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """ A module that create a rectangle and initialize its dimensions"""
 class Rectangle:
-    """ inidialize with the width and height with value checks
-    Args:
-        width: how wide the rectangle is from side to side
-        height: height labels how high (how tall) the rectangle is
-    Return: nonezo
-    """
+    """Represent a rectangle."""
+
     def __int__(self, hiegth=0, width=0):
-        self.heigth =hiegth
+        """ inidialize with the width and height with value checks
+        Args:
+            width: how wide the rectangle is from side to side
+            height: height labels how high (how tall) the rectangle is
+        """
+        self.heigth = hiegth
         self.width = width
 
     '''defining a getter'''
@@ -26,18 +27,15 @@ class Rectangle:
         self.__width=value
 
     '''defining a getter'''
-
     @property
     def heigth(self):
         return self.__heigth
 
     '''defining a setter'''
-
-    @width.setter
+    @heigth.setter
     def heigth(self, value):
         if isinstance(value, int) is False:
             raise TypeError("heigth must be an integer")
         if value < 0:
             raise ValueError("heigth must be >= 0")
         self.__heigth = value
-
--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """ A module that create a rectangle and initialize its dimensions"""
 class Rectangle:
-    """ inidialize with the width and height with value checks
-    Args:
-        width: how wide the rectangle is from side to side
-        height: height labels how high (how tall) the rectangle is
-    Return: nonezo
-    """
+    """Represent a rectangle."""
+
     def __int__(self, hiegth=0, width=0):
+        """ inidialize with the width and height with value checks
+        Args:
+            width: how wide the rectangle is from side to side
+            height: height labels how high (how tall) the rectangle is
+        """
         self.heigth =hiegth
         self.width = width
 
@@ -26,14 +27,12 @@ class Rectangle:
         self.__width=value
 
     '''defining a getter'''
-
     @property
     def heigth(self):
         return self.__heigth
 
     '''defining a setter'''
-
-    @width.setter
+    @heigth.setter
     def heigth(self, value):
         if isinstance(value, int) is False:
             raise TypeError("heigth must be an integer")
